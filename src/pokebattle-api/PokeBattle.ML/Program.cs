e65900bcf12d237c180204cb9749f9fb58f25e23
Matteo Bortolazzo <matteobortolazzo@pm.me>
2025-11-12 11:57:35 +0100

    Add predict files

---
+++ src/pokebattle-api/PokeBattle.ML/Program.cs
@@ -0,0 +1,27 @@
+﻿using PokeBattle.ML;
+
+const string source = "/home/mborto/Repos/PokeBattle/data/pokemon.csv";
+
+// Test with a sample Pokemon
+var testPokemon = new Pokemon
+{
+    Hp = 100,
+    Attack = 125,
+    Defense = 90,
+    SpecialAttack = 60,
+    SpecialDefence = 70,
+    Speed = 85
+};
+/* PREDICT TYPE * /
+/*
+const string modelPath = "/home/mborto/Repos/PokeBattle/data/predict-type.zip";
+var predictTypeModel = new PredictTypeModel(modelPath, source);
+var predictedType = predictTypeModel.PredictTypeModel(testPokemon)
+*/
+
+/* PREDICT TOTAL STATS */
+/*
+const string totalStatsModelPath = "/home/mborto/Repos/PokeBattle/data/predict-total-stats.zip";
+var predictTotalStatsModel = new PredictTotalStatsModel(totalStatsModelPath, source);
+var predictedTotal = predictTotalStatsModel.PredictTotalStat(testPokemon);
+*/
